240b539afa3a8f887279848fc215ecc58160a080
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2022-04-28 22:57:28 +0200

    clear command working fine

--- extensions/Moderation.py
+++ extensions/Moderation.py
@@ -1,10 +1,8 @@
 import disnake
 from disnake.ext import commands
-from disnake.ext.tasks import loop
 import humanfriendly
 import datetime
 import humanize
-import json
 
 
 class Moderation(commands.Cog):
@@ -131,166 +129,140 @@ class Moderation(commands.Cog):
                 ephemeral=True
             )
 
-    @commands.slash_command(
-        name="clear",
-        description="Clears a channel",
-        options=[
-            disnake.Option(
-                name="amount",
-                description="The amount of messages to delete",
-                required=True,
-                min_value=1,
-                max_value=100
-            ),
-            disnake.Option(
-                name="type",
-                description="What kind do you want to clear?",
-                required=True,
-                choices=[
-                    disnake.OptionChoice(
-                        name="embeds",
-                        value="delete all embeds within your limit"
-                    ),
-                    disnake.OptionChoice(
-                        name="pinned",
-                        value="delete all pinned messages within your limit"
-                    ),
-                    disnake.OptionChoice(
-                        name="messages",
-                        value="delete all message within your limit"
-                    ),
-                    disnake.OptionChoice(
-                        name="images",
-                        value="delete all images within your limit"
-                    )
-                ]
-            )
-        ]
-    )
-    @commands.has_permissions(
-        manage_messages=True
-    )
+    @commands.slash_command()
     async def clear(
         self,
-        interaction: disnake.ApplicationCommandInteraction,
-        amount: int,
-        type: str
+        interaction: disnake.CommandInteraction
     ):
+        pass
 
-        if type == "embeds":
-            check = lambda msg: msg.embeds
+    @clear.sub_command()
+    async def embeds(
+        self,
+        interaction: disnake.ApplicationCommandInteraction,
+        amount: int
+    ):
+        check = lambda msg: msg.embeds
 
-            await interaction.channel.purge(
+        await interaction.channel.purge(
 
-                limit=amount,
-                check=check
+            limit=amount,
+            check=check
+        )
+        if amount == 1:
+            purge_embed = disnake.Embed(
+                description="Es wurde `1 Embed` erfolgreich gelöscht!",
+                color=disnake.Color.red()
             )
-            if amount == 1:
-                purge_embed = disnake.Embed(
-                    description="Es wurde `1 Embed` erfolgreich gelöscht!",
-                    color=disnake.Color.red()
-                )
-                await interaction.response.send_message(
-                    embed=purge_embed,
-                    ephemeral=True
-                )
-            else:
-                purge_embed = disnake.Embed(
-                    description=f"Es wurden `{amount} Embeds` erfolgreich gelöscht!",
-                    color=disnake.Color.red()
-                )
-                await interaction.response.send_message(
-                    embed=purge_embed,
-                    ephemeral=True
-                )
-
-        elif type == "pinned":
-            check = lambda msg: msg.pinned and not msg.embeds
-
-            await interaction.channel.purge(
-
-                limit=amount,
-                check=check
+            await interaction.response.send_message(
+                embed=purge_embed,
+                ephemeral=True
             )
-            if amount == 1:
-                purge_embed = disnake.Embed(
-                    description="Es wurde `1 gepinnte Nachricht` erfolgreich gelöscht!",
-                    color=disnake.Color.red()
-                )
-                await interaction.response.send_message(
-                    embed=purge_embed,
-                    ephemeral=True
-                )
-            else:
-                purge_embed = disnake.Embed(
-                    description=f"Es wurden `{amount} gepinnte Nachrichten` erfolgreich gelöscht!",
-                    color=disnake.Color.red()
-                )
-                await interaction.response.send_message(
-                    embed=purge_embed,
-                    ephemeral=True
-                )
+        else:
+            purge_embed = disnake.Embed(
+                description=f"Es wurden `{amount} Embeds` erfolgreich gelöscht!",
+                color=disnake.Color.red()
+            )
+            await interaction.response.send_message(
+                embed=purge_embed,
+                ephemeral=True
+            )
+    @clear.sub_command()
+    async def pinned(
+        self,
+        interaction: disnake.ApplicationCommandInteraction,
+        amount: int
+    ):
+        check = lambda msg: msg.pinned and not msg.embeds
 
-        elif type == "messages":
-            check = lambda msg: not msg.pinned and not msg.embeds
+        await interaction.channel.purge(
 
-            await interaction.channel.purge(
-
-                limit=amount,
-                check=check
+            limit=amount,
+            check=check
+        )
+        if amount == 1:
+            purge_embed = disnake.Embed(
+                description="Es wurde `1 gepinnte Nachricht` erfolgreich gelöscht!",
+                color=disnake.Color.red()
+            )
+            await interaction.response.send_message(
+                embed=purge_embed,
+                ephemeral=True
+            )
+        else:
+            purge_embed = disnake.Embed(
+                description=f"Es wurden `{amount} gepinnte Nachrichten` erfolgreich gelöscht!",
+                color=disnake.Color.red()
+            )
+            await interaction.response.send_message(
+                embed=purge_embed,
+                ephemeral=True
             )
 
-            if amount == 1:
-                purge_embed = disnake.Embed(
-                    description="Es wurde `1 Nachricht` erfolgreich gelöscht!",
-                    color=disnake.Color.red()
-                )
-                await interaction.response.send_message(
-                    embed=purge_embed,
-                    ephemeral=True
-                )
-            else:
-                purge_embed = disnake.Embed(
-                    description=f"Es wurden `{amount} Nachrichten` erfolgreich gelöscht!",
-                    color=disnake.Color.red()
-                )
-                await interaction.response.send_message(
-                    embed=purge_embed,
-                    ephemeral=True
-                )
+    @clear.sub_command()
+    async def messages(
+        self,
+        interaction: disnake.ApplicationCommandInteraction,
+        amount: int
+    ):
+        check = lambda msg: not msg.pinned and not msg.embeds
 
-        elif type == "images":
-            check = lambda msg: not msg.pinned and msg.attachments
+        await interaction.channel.purge(
 
-            await interaction.channel.purge(
+            limit=amount,
+            check=check
+        )
 
-                limit=amount,
-                check=check
+        if amount == 1:
+            purge_embed = disnake.Embed(
+                description="Es wurde `1 Nachricht` erfolgreich gelöscht!",
+                color=disnake.Color.red()
             )
-            if amount == 1:
-                purge_embed = disnake.Embed(
-                    description="Es wurde `1 Bild` erfolgreich gelöscht!",
-                    color=disnake.Color.red()
-                )
-                await interaction.response.send_message(
-                    embed=purge_embed,
-                    ephemeral=True
-                )
-            else:
-                purge_embed = disnake.Embed(
-                    description=f"Es wurden `{amount} Bilder` erfolgreich gelöscht!",
-                    color=disnake.Color.red()
-                )
-                await interaction.response.send_message(
-                    embed=purge_embed,
-                    ephemeral=True
-                )
+            await interaction.response.send_message(
+                embed=purge_embed,
+                ephemeral=True
+            )
+        else:
+            purge_embed = disnake.Embed(
+                description=f"Es wurden `{amount} Nachrichten` erfolgreich gelöscht!",
+                color=disnake.Color.red()
+            )
+            await interaction.response.send_message(
+                embed=purge_embed,
+                ephemeral=True
+            )
 
-    @loop(seconds=90)
-    async def check_streams(self):
-        with open("json/guild.json", "r", encoding="UTF-8") as f:
-            guild_data = json.load(f)
+    @clear.sub_command()
+    async def images(
+        self,
+        interaction: disnake.ApplicationCommandInteraction,
+        amount: int
+    ):
+        check = lambda msg: not msg.pinned and msg.attachments
 
+        await interaction.channel.purge(
+
+            limit=amount,
+            check=check
+        )
+        if amount == 1:
+            purge_embed = disnake.Embed(
+                description="Es wurde `1 Bild` erfolgreich gelöscht!",
+                color=disnake.Color.red()
+            )
+            await interaction.response.send_message(
+                embed=purge_embed,
+                ephemeral=True
+            )
+        else:
+            purge_embed = disnake.Embed(
+                description=f"Es wurden `{amount} Bilder` erfolgreich gelöscht!",
+                color=disnake.Color.red()
+            )
+            await interaction.response.send_message(
+                embed=purge_embed,
+                ephemeral=True
+            )
 
 
 def setup(bot):
