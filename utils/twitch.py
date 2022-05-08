30bfc9d4d409ab0bcc27f4879e6372282c6e17b3
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2022-05-08 02:59:56 +0200

    removed unused functions

--- utils/twitch.py
+++ utils/twitch.py
@@ -1,6 +1,4 @@
 import json
-import time
-from datetime import datetime
 import requests
 
 
@@ -29,24 +27,6 @@ def get_users(login_names):
     return {entry["login"]: entry["id"] for entry in response.json()["data"]}
 
 
-def get_profile_image(login_names):
-    params = {
-        "login": login_names
-    }
-
-    headers = {
-        "Authorization": f'Bearer {config["twitch"]["access_token"]}',
-        "Client-Id": config["twitch"]["client_id"]
-    }
-
-    response = requests.get(
-        "https://api.twitch.tv/helix/users",
-        params=params,
-        headers=headers
-    )
-    return {entry["login"]: entry["profile_image_url"] for entry in response.json()["data"]}
-
-
 def get_streams(users):
     params = {
         "user_id": users.values()
@@ -62,23 +42,3 @@ def get_streams(users):
         headers=headers
     )
     return {entry["user_login"]: entry for entry in response.json()["data"]}
-
-
-online_users = []
-
-
-def get_notifications():
-    global online_users
-    users = get_users(watchlist_data["overall_watchlist"])
-    streams = get_streams(users)
-
-    notifications = []
-    for user_name in watchlist_data["overall_watchlist"]:
-        if user_name in streams and user_name not in online_users:
-            giga_time = datetime.strptime(streams[user_name]['started_at'], "%Y-%m-%dT%H:%M:%SZ")
-            started_at = time.mktime(giga_time.timetuple()) + giga_time.microsecond / 1E6
-            if time.time() - started_at < 4000:
-                notifications.append(streams[user_name])
-                online_users.append(user_name)
-
-    return notifications
