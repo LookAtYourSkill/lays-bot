424341bf65ea14480e333303428b1f3141fdc790
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2022-07-01 22:25:01 +0200

    added license test

--- extensions/About.py
+++ extensions/About.py
@@ -1,3 +1,5 @@
+import json
+
 import disnake
 from disnake.ext import commands
 
@@ -18,37 +20,56 @@ class About(commands.Cog):
         self,
         interaction: disnake.ApplicationCommandInteraction
     ):
-        loading_embed = disnake.Embed(
-            description="Fetching information about the bot...",
-            color=disnake.Color.green()
-        )
-        await interaction.response.send_message(
-            embed=loading_embed,
-            ephemeral=True
-        )
-        owner = self.bot.get_user(493370963807830016)
+        with open("json/licenses.json", "r") as license_info:
+            licenses = json.load(license_info)
+        with open("json/guild.json", "r") as guild_info:
+            guilds = json.load(guild_info)
+        with open("json/general.json", "r") as general_info:
+            general = json.load(general_info)
 
-        about_embed = disnake.Embed(
-            color=interaction.author.color
-        )
-        about_embed.add_field(
-            name="> ❓ Autor",
-            value=f"`»` Der Bot wurde von `{owner}` geschrieben",
-            inline=False
-        )
-        about_embed.add_field(
-            name="> ❓ Informationen",
-            value=f"`»` Momentane Version: `{self.version}`\n"
-                  f"`»` Momentanes Prefix: `{self.prefix}`",
-            inline=False
-        )
-        about_embed.set_author(
-            name=owner,
-            icon_url=owner.avatar.url
-        )
-        await interaction.edit_original_message(
-            embed=about_embed
-        )
+        if not guilds[str(interaction.author.guild.id)]["license"] or guilds[str(interaction.author.guild.id)]["license"] not in licenses and not general["license_check"]:
+            no_licesnse_embed = disnake.Embed(
+                title="No license ⛔",
+                description="You have not set a license for this server. Please use `/license activate <license>` to set a license.",
+                color=disnake.Color.red()
+            )
+            await interaction.response.send_message(
+                embed=no_licesnse_embed,
+                ephemeral=True
+            )
+
+        else:
+            loading_embed = disnake.Embed(
+                description="Fetching information about the bot...",
+                color=disnake.Color.green()
+            )
+            await interaction.response.send_message(
+                embed=loading_embed,
+                ephemeral=True
+            )
+            owner = self.bot.get_user(493370963807830016)
+
+            about_embed = disnake.Embed(
+                color=interaction.author.color
+            )
+            about_embed.add_field(
+                name="> ❓ Autor",
+                value=f"`»` Der Bot wurde von `{owner}` geschrieben",
+                inline=False
+            )
+            about_embed.add_field(
+                name="> ❓ Informationen",
+                value=f"`»` Momentane Version: `{self.version}`\n"
+                        f"`»` Momentanes Prefix: `{self.prefix}`",
+                inline=False
+            )
+            about_embed.set_author(
+                name=owner,
+                icon_url=owner.avatar.url
+            )
+            await interaction.edit_original_message(
+                embed=about_embed
+            )
 
 
 def setup(bot):
