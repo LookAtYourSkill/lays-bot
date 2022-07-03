e9f4c71f5965eed1d0be97665fb908cf3dba0d1e
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2022-07-03 03:42:46 +0200

    testing with class 'Help2'

--- extensions/help.py
+++ extensions/help.py
@@ -38,16 +38,6 @@ class Help(commands.Cog):
             color=interaction.author.color
         )
 
-        # cog_name = [name for name in self.bot.cogs]
-        # cog_with_cmd = [cog_name for cog in self.bot.cogs.values() if len(cog.get_commands()) > 0]
-
-        # for _cog in cog_with_cmd:
-        #     help_embed.add_field(
-        #         name=f"{_cog.qualified_name}",
-        #         value=f"{_cog.description}",
-        #         inline=False
-        #     )
-
         if not cog:  # and not command:
             cog_filter = [
                 cog for cog in self.bot.cogs.values() if len(cog.get_application_commands()) > 0
@@ -96,132 +86,71 @@ class Help(commands.Cog):
                     embed=cog_embed
                 )
 
-                # for command in __cog.get_application_commands():
-                #     cog_embed.add_field(
-                #         name=f"- {command.qualified_name}",
-                #         # value=f"`»` `{command.description if command.description else 'No description'}`",
-                #         value="`»` `No description`",
-                #         inline=False
-                #     )
 
-        # elif command and not cog:
-            # return
+class Help2(commands.Cog):
+    def __init__(
+        self,
+        bot
+    ):
+        self.bot = bot
 
-            # cmd = self.bot.get_slash_command(command)
+    @commands.slash_command(
+        name="help2",
+        description="Shows help for a command"
+    )
+    async def help(
+        self,
+        interaction: disnake.ApplicationCommandInteraction,
+        cog: (Optional[str]) = None,
+        command: (Optional[str]) = None
+    ):
+        if not command and not cog:
+            embed = disnake.Embed(
+                title="Help",
+                description="This is the help command. Use `/help <command>` to get help for a command.",
+                color=interaction.author.color
+            )
 
-            # help_embed = disnake.Embed(
-            #     title=f"__{cmd.qualified_name}__",
-            #     description=f"`»` `{cmd.description}`",
-            #     color=interaction.author.color
-            # )
+            for cog in self.bot.cogs.values():
+                if len(cog.get_commands()) > 0:
+                    embed.add_field(
+                        name=f"__{cog.qualified_name}__",
+                        value=f"`{cog.description if cog.description else 'No description'}`",
+                        inline=False
+                    )
 
-            # if cmd.subcommand_group:
-            #     help_embed.add_field(
-            #         name="__Subcommands__",
-            #         value="\n - ".join(f"`{cmd.qualified_name}`"),
-            #         inline=False
-            #     )
-            #     await interaction.edit_original_message(
-            #         embed=help_embed
-            #     )
+            await interaction.response.send_message(
+                embed=embed,
+                ephemeral=True
+            )
 
-            # else:
-            # help_embed.add_field(
-            #     name="> ❓ Syntax",
-            #     value=f"`»` `/{cmd.qualified_name}`",
-            #     inline=False
-            # )
-            # await interaction.edit_original_message(
-            #     embed=help_embed
-            # )
+        if not command:
+            __cog = self.bot.get_cog(cog)
 
-        # else:
-        #     pass
-
-    class Help2(commands.Cog):
-        def __init__(
-            self,
-            bot
-        ):
-            self.bot = bot
-
-        @commands.slash_command(
-            name="help2",
-            description="Shows help for a command"
-        )
-        async def help(
-            self,
-            interaction: disnake.ApplicationCommandInteraction,
-            cog: (Optional[str]) = None,
-            command: (Optional[str]) = None
-        ):
-            if not command and not cog:
-                embed = disnake.Embed(
-                    title="Help",
-                    description="This is the help command. Use `/help <command>` to get help for a command.",
-                    color=interaction.author.color
+            if not __cog:
+                error_embed = disnake.Embed(
+                    description=f"Did not found the cog, you requested (`{cog}`)",
+                    color=disnake.Color.red()
                 )
-
-                for cog in self.bot.cogs.values():
-                    if len(cog.get_commands()) > 0:
-                        embed.add_field(
-                            name=f"__{cog.qualified_name}__",
-                            value=f"`{cog.description if cog.description else 'No description'}`",
-                            inline=False
-                        )
-
                 await interaction.response.send_message(
-                    embed=embed,
+                    embed=error_embed,
                     ephemeral=True
                 )
 
-            if not command:
-                __cog = self.bot.get_cog(cog)
-
-                if not __cog:
-                    error_embed = disnake.Embed(
-                        description=f"Did not found the cog, you requested (`{cog}`)",
-                        color=disnake.Color.red()
-                    )
-                    await interaction.response.send_message(
-                        embed=error_embed,
-                        ephemeral=True
-                    )
-
-                else:
-                    embed = disnake.Embed(
-                        title=f"__{__cog.qualified_name}__",
-                        description=f"`{__cog.description if __cog.description else 'No description'}`",
-                        color=interaction.author.color
-                    )
-
-                    for command in __cog.walk_commands():
-                        embed.add_field(
-                            name=f"- {command.qualified_name}",
-                            value=f"`»` `{command.description if command.description else 'No description'}`",
-                            inline=False
-                        )
-
-                    await interaction.response.send_message(
-                        embed=embed,
-                        ephemeral=True
-                    )
-
-            elif not cog:
-                return
-                cmd = self.bot.get_slash_command(command)
-
+            else:
                 embed = disnake.Embed(
-                    title=f"__{cmd.parent}__",
-                    description=f"`{[cmd.description if cmd.description else 'No description']}`",
+                    title=f"__{__cog.qualified_name}__",
+                    description=f"`{__cog.description if __cog.description else 'No description'}`",
                     color=interaction.author.color
                 )
 
-                embed.add_field(
-                    name="__Subcommands__",
-                    value="\n - ".join(f"`{cmd.qualified_name}`"),
-                    inline=False
-                )
+                for command in __cog.walk_commands():
+                    embed.add_field(
+                        name=f"- {command.qualified_name}",
+                        value=f"`»` `{command.description if command.description else 'No description'}`",
+                        inline=False
+                    )
+
                 await interaction.response.send_message(
                     embed=embed,
                     ephemeral=True
@@ -230,3 +159,4 @@ class Help(commands.Cog):
 
 def setup(bot):
     bot.add_cog(Help(bot))
+    bot.add_cog(Help2(bot))
