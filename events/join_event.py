293f8845ea28f31148994ad347d7f2a692b6a241
LookAtYourSKill <66884858+LookAtYourSKill@users.noreply.github.com>
2022-09-06 14:13:30 +0200

    dont check bot

--- events/join_event.py
+++ events/join_event.py
@@ -1,3 +1,4 @@
+from types import NoneType
 import disnake
 from disnake.ext import commands
 import json
@@ -9,6 +10,9 @@ class join_to_create(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: disnake.Member, before: disnake.VoiceState, after: disnake.VoiceState):
+        if member.id == self.bot.user.id:
+            return
+
         with open("json/join_to_create.json", "r") as json_file:
             data = json.load(json_file)
         with open("json/guild.json", "r") as guild_file:
@@ -23,24 +27,27 @@ class join_to_create(commands.Cog):
                 member.guild.categories,
                 id=guild_data[str(member.guild.id)]["join_to_create_category"]
             )
-            if after.channel.id == channel.id:
-                new_voice = await member.guild.create_voice_channel(
-                    name=f"║🔊・{member.name}'s channel",
-                    category=category,
-                    overwrites={
-                        member: disnake.PermissionOverwrite(
-                            administrator=True
-                        )
-                    }
-                )
-                await member.move_to(new_voice)
-                data["jtcc"][str(new_voice.id)] = {}
-                data["jtcc"][str(new_voice.id)]["owner"] = member.id
-                data["jtcc"][str(new_voice.id)]["channel_name"] = new_voice.name
-                data["jtcc"][str(new_voice.id)]["channel_id"] = new_voice.id
-                data["jtcc"][str(new_voice.id)]["state"] = "public"
-                with open("json/join_to_create.json", "w") as json_file:
-                    json.dump(data, json_file, indent=4)
+            try:
+                if after.channel.id == channel.id:
+                    new_voice = await member.guild.create_voice_channel(
+                        name=f"║🔊・{member.name}'s channel",
+                        category=category,
+                        overwrites={
+                            member: disnake.PermissionOverwrite(
+                                administrator=True
+                            )
+                        }
+                    )
+                    await member.move_to(new_voice)
+                    data["jtcc"][str(new_voice.id)] = {}
+                    data["jtcc"][str(new_voice.id)]["owner"] = member.id
+                    data["jtcc"][str(new_voice.id)]["channel_name"] = new_voice.name
+                    data["jtcc"][str(new_voice.id)]["channel_id"] = new_voice.id
+                    data["jtcc"][str(new_voice.id)]["state"] = "public"
+                    with open("json/join_to_create.json", "w") as json_file:
+                        json.dump(data, json_file, indent=4)
+            except AttributeError:
+                print("NoneType")
 
         elif len(before.channel.members) == 0:  # and after.channel is None:
             if str(before.channel.id) in data["jtcc"]:
