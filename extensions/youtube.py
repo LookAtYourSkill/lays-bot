c6318a7c762058fcd3818166f4d9fc52c0997fbc
LookAtYourSkill <callmesleeplxss@gmail.com>
2023-01-23 14:49:59 +0100

    added whole youtube notification (might work)

--- extensions/youtube.py
+++ extensions/youtube.py
@@ -1,41 +1,145 @@
-# -*- coding: utf-8 -*-
+import json
+from datetime import datetime
 
-# Sample Python code for youtube.playlists.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/code-samples#python
+import disnake
+from disnake.ext import commands
+from disnake.ext.tasks import loop
 
-import os
+from utils.youtube import (check_title, convert_usernamne_to_id,
+                           get_latest_videos, is_short)
 
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
 
-scopes = ["https://www.googleapis.com/auth/youtube"]
+class Youtube(commands.Cog):
+    def __init__(self, bot):
+        self.bot: disnake.Client = bot
+        self.youtube.start()
 
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    @loop(seconds=1800)
+    async def youtube(self):
+        # open json file
+        with open("json/youtube.json", "r") as data_file:
+            data: dict = json.load(data_file)
 
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "etc/youtube-config.json"
+        with open("json/guild.json", "r") as data_file:
+            guild: dict = json.load(data_file)
+        
+        # catch all errors
+        try:
+            # check if data is not empty
+            if data:
+                print(data)
+                # get every entry
+                for youtuberName in data:
+                    print(youtuberName)
+                    # check if entry is not empty
+                    if youtuberName:
+                        # get every guild in data
+                        for discordServer in data[youtuberName]:
+                            print(discordServer)
+                            # check if data is in the guild
+                            if discordServer:
+                                if youtuberName in guild[discordServer]["youtube-watchlist"]:
+                                    print(f"{youtuberName} is in {discordServer} watchlist")
+                                    # define basic variables
+                                    phrase = data[youtuberName][discordServer]["phrase"]
+                                    checkTime = data[youtuberName][discordServer]["checkTime"]
+                                    
+                                    # define other variables
+                                    publishedAt = data[youtuberName][discordServer][youtuberName]["publishedAt"]
+                                    # title = data[youtuberName][discordServer]["videoTitle"]
+                                    # thumbnail = data[youtuberName][discordServer]["thumbnail"]
+                                    # videoId = data[youtuberName][discordServer]["videoId"]
+                                    channelId = convert_usernamne_to_id(data[youtuberName][discordServer][youtuberName]["channelName"])
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+                                    # get latest video
+                                    newVideo = get_latest_videos(channelId)
+                                    newVideoId = newVideo[0]["id"]["videoId"]
+                                    newPublishedAt = newVideo[0]["snippet"]["publishedAt"]
 
-    request = youtube.playlists().list(
-        part="snippet,contentDetails",
-        channelId="UCTNSn6kOzoLllZeWUzI_bqw",
-        maxResults=25
-    )
-    response = request.execute()
+                                    # check if video is short and if the title contains the phrase
+                                    if is_short(newVideoId, checkTime) or check_title(newVideoId, phrase):
+                                        print("video is short or the title contains the phrase")
+                                        # video is short or the title contains the phrase
+                                        # therefore there is no new video
+                                        pass
+                                    else:
+                                        print("video is not short and the title doesnt contain the phrase")
+                                        # video is not short and the title doesnt contain the phrase
+                                        # therefore there is a new video
+                                        
+                                        # check if video is already in the json file
+                                        if newVideoId not in data:
+                                            print("video is not in the json file")
+                                            # video is not in the json file
+                                            # therefore there is a new video
+                                            # to double check if there is a new video -> compare the publishedAt date
 
-    print(response)
+                                            timestamp1 = publishedAt
+                                            timestamp2 = newPublishedAt
 
-if __name__ == "__main__":
-    main()
+                                            oldTimeStamp = datetime.strptime(timestamp1, "%Y-%m-%dT%H:%M:%SZ")
+                                            newTimeStamp = datetime.strptime(timestamp2, "%Y-%m-%dT%H:%M:%SZ")
+
+                                            # check if the publishedAt date is the same
+                                            if oldTimeStamp != newTimeStamp:
+                                                print("oldTimeStamp is not the same as newTimeStamp")
+                                                if oldTimeStamp < newTimeStamp:
+                                                    print("oldTimeStamp is older than newTimeStamp")
+                                                    # oldTimeStamp is older than newTimeStamp
+                                                    # therefore there is a new video
+
+                                                    # clear the json file
+                                                    data[youtuberName][discordServer][youtuberName] = {}
+
+                                                    # write to json file
+                                                    with open("json/youtube.json", "w") as data_file:
+                                                        json.dump(data, data_file, indent=4)
+
+                                                    # define variables
+                                                    data: dict = data
+
+                                                    # add video to json file
+                                                    data[youtuberName][discordServer][youtuberName] = {}
+                                                    data[youtuberName][discordServer][youtuberName]["publishedAt"] = newVideo[0]["snippet"]["publishedAt"]
+                                                    data[youtuberName][discordServer][youtuberName]["videoTitle"] = newVideo[0]["snippet"]["title"]
+                                                    data[youtuberName][discordServer][youtuberName]["thumbnail"] = newVideo[0]["snippet"]["thumbnails"]["high"]["url"]
+                                                    data[youtuberName][discordServer][youtuberName]["videoId"] = newVideoId
+                                                    data[youtuberName][discordServer][youtuberName]["channelName"] = data[youtuberName][discordServer]["channelName"]
+
+                                                    # write to json file
+                                                    with open("json/youtube.json", "w") as data_file:
+                                                        json.dump(data, data_file, indent=4)
+                                                else:
+                                                    print("oldTimeStamp is newer than newTimeStamp")
+                                                    # oldTimeStamp is newer than newTimeStamp
+                                                    # therefore there is no new video
+                                                    pass
+                                            else:
+                                                print("oldTimeStamp is the same as newTimeStamp")
+                                                # oldTimeStamp is the same as newTimeStamp
+                                                # therefore there is no new video
+                                                pass
+                                        else:
+                                            print("video is in the json file")
+                                            # video is in the json file
+                                            # there is no new video
+                                            pass
+                                else:
+                                    print(f"{youtuberName} is not in the guild youtube watchlist")
+                                    # youtuber is not in the guild youtube watchlist
+                                    pass
+                        else:
+                            # there is no data in the guild
+                            pass
+                else:
+                    # there is no entry
+                    pass
+            else:
+                # there is no data
+                pass        
+        # catch error if there is no entry
+        except KeyError as e:
+            print(e)
+
+def setup(bot):
+    bot.add_cog(Youtube(bot))
---
+++ utils/youtube.py
@@ -0,0 +1,190 @@
+import json
+
+import colorama
+import requests
+from isodate import parse_duration
+
+with open("json/youtube-watchlist.json", 'r', encoding='UTF-8') as data_file:
+    watchlist_data = json.load(data_file)
+
+with open('json/youtube-config.json', 'r', encoding='UTF-8') as config_file:
+    config = json.load(config_file)
+
+# ! WORKING
+def convert_usernamne_to_id(username, api_key = config["installed"]["client_secret"]):
+    # make request to youtube api with search parameters
+    url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={username}&type=channel&key={api_key}"
+    # get response from request
+    response = requests.get(url)
+    # convert response to json
+    data = response.json()
+    # start try block
+    try:
+        # check if request was successful
+        if data["items"]:
+            # get the user id from the response
+            user_id = data['items'][0]['snippet']['channelId']
+            # return the user id
+            return user_id
+        # if no user is found
+        else:
+            # return error message
+            print(f"No channel found for username. maybe typo in username")
+            return None
+    except KeyError as e:
+        print(f"{colorama.Fore.RED} [CONVERT ERROR] {e} {colorama.Fore.RESET}")
+
+# ! WORKING
+def get_channel_info(channel_id, api_key = config["installed"]["client_secret"]):
+    # make request to youtube api for channel info
+    url = f"https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={channel_id}&key={api_key}"
+    # get response from request
+    response = requests.get(url)
+    # convert response to json
+    data = response.json()
+    # start exception check
+    try:
+        # check if request was successful
+        if response.status_code == 200:
+            # check if data is found
+            if data["items"]:
+                # return the data
+                return data["items"]
+            else:
+                # returns error message if no data is found
+                print("No data found. Channel may not exist or typo in username")
+                return None
+        else:
+            # returns error message if request fails
+            print(f"{colorama.Fore.RED} [CHANNEL INFO ERROR] {response.status_code} | Message: {response.json()} {colorama.Fore.RESET}")
+    # catch exception if data is not found
+    except KeyError as e:
+        # print error message
+        print(f"{colorama.Fore.RED} [CHANNEL INFO ERROR] {e} {colorama.Fore.RESET}")
+
+# ! WORKING
+def get_latest_videos(channel_id, api_key = config["installed"]["client_secret"]):
+    # make request to youtube api with search parameters
+    url = f"https://www.googleapis.com/youtube/v3/search?key={api_key}&channelId={channel_id}&part=snippet,id&order=date&maxResults=1"
+    # get response from request
+    response = requests.get(url)
+    # convert response to json
+    data = response.json()
+    # start exception check
+    try:
+        # check if request was successful
+        if response.status_code == 200:
+            # check if data is found
+            if data["items"]:
+                # return the data
+                return data["items"]
+            else:
+                print("No data found. Channel may not exist or typo in username")
+                # returns error message if no data is found
+                return None
+        else:
+            # returns error message if request fails
+            print(f"{colorama.Fore.RED} [LAST VIDEOS ERROR] {response.status_code} | Message: {response.json()} {colorama.Fore.RESET}")
+    # catch exception if data is not found
+    except KeyError as e:
+        # print error message
+        print(f"{colorama.Fore.RED} [LAST VIDEOS ERROR] {e} {colorama.Fore.RESET}")
+
+
+# ! WORKING 
+def is_short(video_id: str, checkTime: int = 60, api_key = config["installed"]["client_secret"]):
+    # make request to youtube api with search parameters
+    url = f"https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id={video_id}&key={api_key}"
+    # get response from request
+    response = requests.get(url)
+    # convert response to json
+    data = response.json()
+    # get the duration of the video
+    video_duration = data['items'][0]['contentDetails']['duration']
+    # convert duration to seconds
+    duration = parse_duration(video_duration)
+    # convert duration to seconds
+    total_seconds = int(duration.total_seconds()) 
+    # check if video duration is less than the checkTime parameter seconds
+    if total_seconds <= checkTime:
+        # * video is short
+        # return true if video is less than checkTime parameter seconds
+        return True
+    else:
+        # * video is no short
+        # return false if video is more than checkTime parameter seconds
+        return False
+
+# ! seems working
+def check_title(video_id: str, sentence: str = "Tjan Reaktion", api_key = config["installed"]["client_secret"]):
+    # make request to youtube api with search parameters
+    url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet&id={video_id}&key={api_key}"
+    # get response from request
+    response = requests.get(url)
+    # convert response to json
+    data = response.json()
+    # get the title of the video
+    video_title: str = data['items'][0]['snippet']['title']
+    # check if sentence is in the title of the video
+    if sentence.lower() in video_title.lower():
+        # sentence is in title
+        # return true if sentence is in the title of the video
+        return True
+    else:
+        # sentence is not in title
+        # return false if sentence is not in the title of the video
+        return False
+
+
+# TODO : Compare timestamps which video was released earlier
+# ! until now it works fine
+def checkVideo(userName: str, phrase: str, checkTime: int = 60):
+    with open("json/youtube.json", "r") as data_file:
+        data = json.load(data_file)
+
+    # define variables
+    phrase = phrase
+    checkTime = checkTime
+
+    # get latest video
+    video = get_latest_videos(convert_usernamne_to_id(userName))
+
+    # check if video is short and if the title contains the phrase
+    if is_short(video[0]["id"]["videoId"], checkTime) or check_title(video[0]["id"]["videoId"], phrase):
+        return None
+    else:
+        # video is not short and the title doesnt contain the phrase
+        # check if video is already in the json file
+        if video[0]["id"]["videoId"] not in data:
+            # video is not in the json file
+            # therefore there is a new video
+            
+            # clear the json file
+            data = {}
+
+            # write to json file
+            with open("json/youtube.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+
+            # define variables
+            data: dict = data
+            videoID = video[0]["id"]["videoId"]
+
+
+            # ! THERE IS A NEW JSON FORMAT SO WOULD NOT WORK WITH THE OLD ONE
+            # add video to json file
+            data[videoID] = {}
+            data[videoID]["publishedAt"] = video[0]["snippet"]["publishedAt"]
+            data[videoID]["title"] = video[0]["snippet"]["title"]
+            data[videoID]["thumbnail"] = video[0]["snippet"]["thumbnails"]["high"]["url"]
+            data[videoID]["videoId"] = videoID
+
+            # write to json file
+            with open("json/youtube.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+
+            return data[videoID]
+        
+        else:
+            # there is no new video
+            return None
